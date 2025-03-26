a754af7dc7e6e23f69c4d111d7e89bbbe9b6fa37
Dmitrii <73591672+bushuevda@users.noreply.github.com>
2025-03-26 19:43:17 +0300

    Update example.js

--- js/file_manager/example.js
+++ js/file_manager/example.js
@@ -1,6 +1,6 @@
 const {file_manager} = require ('./file_manager/file_manager');
 
-arr = []
-file_manager.findFile("aaa.txt", arr, dir = './')
+arr = [];
+file_manager.findFile("aaa.txt", arr, dir = './');
 
 file_manager.aboutFile(file_manager.parsePathFile(arr[0]));
